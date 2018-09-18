9c23a43829b0d209d83bfce2a7be004b12e4b957
jade.paukkunen <jademariia.paukkunen@gmail.com>
2018-09-18 13:23:09 +0300

    Hei vaan, minä tein tässä hassun muutoksen

--- src/Default/Main.java
+++ src/Default/Main.java
@@ -2,6 +2,6 @@ package Default;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello pumpernikkeli");
+        System.out.println("Hello kana");
     }
 }
